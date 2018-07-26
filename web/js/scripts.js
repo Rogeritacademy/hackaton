426686fefffa9b97da2915098bd0da6dd499d89a
Roger Masellas <roger@lamosca.com>
2018-07-26 20:23:50 +0200

    primer de la branca

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,6 +1,8 @@
 (function($) {
     $(function() {
 
+/** Animació elements entrada **/
+
         var elasticity = anime.timeline();
 
         elasticity.add({
@@ -16,6 +18,33 @@
                 offset: 0,
                 delay: 500,
                 duration: 1000,
+                opacity: 1,
+            })
+            .add({
+                targets: '#elasticity .inputfila01',
+                translateX: 0,
+                offset: 0,
+                delay: 800,
+                duration: 1000,
+                elasticity: 200,
+                opacity: 1,
+            })
+            .add({
+                targets: '#elasticity .inputfila02',
+                translateX: 0,
+                offset: 0,
+                delay: 800,
+                duration: 1000,
+                elasticity: 200,
+                opacity: 1,
+            })
+            .add({
+                targets: '#elasticity .inputfila03',
+                translateX: 0,
+                offset: 0,
+                delay: 800,
+                duration: 1000,
+                elasticity: 200,
                 opacity: 1,
             })
             .add({
@@ -38,79 +67,89 @@
             })
             .add({
                 targets: '#elasticity .logoent',
-                translateY: 0,
                 offset: 0,
                 delay: 1200,
-                duration: 500,
-                elasticity: 200,
+                duration: 2500,
+                elasticity: 0,
                 opacity: 1,
                 complete: function(anim) {
                     animation();
                 }
             });
-    }); // end of document ready
 
+/** Fi Animació elements entrada **/
 
-    /*** Json ***/
-
-    var url="https://itacademybcn.herokuapp.com/hackaton";
-
-    $("#preview").click(function() {
-        var success=function(user){
-            alert("Benvingut " + user.name);
-        };
-      
-        var error=function(error){
-            alert("Error al registrar-se: " + error.status);
-        };
-        var json = initRegisterJson();
-        debugger;
-        post(url+"/users",json,success,error);
     });
 
-    function initRegisterJson(){
-        //var email=$('#inputEmail').val();
-        //var password=$('#inputPassword').val();
-
-        var nom = $(this).find("input[id='name']").val();
-        var cognoms = $(this).find("input[id='lastName']").val();
-        var email = $(this).find("input[id='email']").val();
-        var password = $(this).find("input[id='password']").val();
-        var idProf = parseInt($(this).find("input[id='idProfile']").val());
-     
-        var json={};
-
-        json["nom"]=nom;
-        json["lastName"]=cognoms;
-        json["email"]=email;
-        json["password"]=password;
-        json["idProfile"]=idProf;
-     
-        return json;
-    }
+// end of document ready
 
 
-    function post(url,json,success, error){
-        $.ajax({
-          method: "POST",
-          url: url,
-          data: JSON.stringify(json),
-          contentType: "application/json"
-        }).done(function(data) {
-           success(data);
-        }).fail(function(err){
-           error(err);
+/*** Json ***/
+
+    $("#preview").click(function() {
+        //var arr = {};
+        var arr = [];
+
+        $(".fieldwrapper").each(function() {
+            var entry = {};
+            var nom = $(this).find("input[id='name']").val();
+            var cognoms = $(this).find("input[id='lastName']").val();
+            var email = $(this).find("input[id='email']").val();
+            var pass = $(this).find("input[id='password']").val();
+            var idProf = parseInt($(this).find("input[id='idProfile']").val());
+            entry.name = nom;
+            entry.lastName = cognoms;
+            entry.email = email;
+            entry.password = pass;
+            entry.idProfile = idProf;
+            arr.push(entry);
         });
-    }
 
-    /*** Fi Json ***/
+        //alert(JSON.stringify(arr));
+        formData=JSON.stringify(arr);
+        formData = formData.replace(/[\[\]]/g, "");
+        //alert(formData);
+
+        $.ajax({
+            type: "POST",
+            url: "https://cors-anywhere.herokuapp.com/https://itacademybcn.herokuapp.com/hackaton/users",
+            data: formData,
+            success: function() { alert('ok!'); },
+            error: function (request, status, error) { alert(formData); },
+            contentType : "application/json"
+        });
+    });
+
+
+    /*
+        $("#add").click(function() {
+            var intId = $("#buildyourform div").length + 1;
+            var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
+            var name = $("<input type=\"text\" name=\"neighborhood\" placeholder=\"Name of Neighborhood\"class=\"fieldname\" />");
+               var url = $("<input type=\"text\" name=\"url\" placeholder=\"Paste here the URL of the Image\"class=\"fieldname\" />");
+
+            var removeButton = $("<input type=\"button\"class=\"remove\" value=\"Remove\" />");
+            removeButton.click(function() {
+                $(this).parent().remove();
+            });
+            fieldWrapper.append(name);
+                    fieldWrapper.append(url);
+
+            fieldWrapper.append(removeButton);
+            $("#buildyourform").append(fieldWrapper);
+        });
+    */
+
+/*** Fi Json ***/
+
+/** Efecte fons **/
 
     var randomMovement = function() {
-        return anime.random(0, 0) + 'rem'
+        return anime.random(0, 0) + 'rem';
     };
 
     var randomSpeed = function() {
-        return anime.random(0, 10000) + 'rem'
+        return anime.random(0, 10000) + 'rem';
     };
 
     function animation() {
